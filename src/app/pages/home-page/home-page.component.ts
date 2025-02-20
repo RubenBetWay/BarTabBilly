ebab046812e8fcf917e4f7052b05559d441849d3
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-20 21:52:21 +0200

    Adding ripple buttons to home page - non-functional

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ButtonConfig, ButtonSize } from 'src/app/shared/components/button/button.model';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+  menuOptions: ButtonConfig[] = [
+    {text: 'Order Round', size: ButtonSize.Large},
+    {text: 'Settle Tab', size: ButtonSize.Large}
+  ]
 }
