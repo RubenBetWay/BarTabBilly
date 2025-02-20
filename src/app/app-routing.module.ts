0e773517060188578fa912321dc5c9e80b17ec9d
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-20 22:04:06 +0200

    Button navigation added as well as Order and Settle Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { OrderRoundPageComponent } from './pages/order-round-page/order-round-page.component';
+import { SettleTabPageComponent } from './pages/settle-tab-page/settle-tab-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent, title: 'Bar Tab Billy' },
   { path: 'home', component: HomePageComponent, title: 'BTB | Home' },
-
+  { path: 'order', component: OrderRoundPageComponent, title: 'BTB | Order' },
+  { path: 'settle', component: SettleTabPageComponent, title: 'BTB | Settle' },
 ];
 
 @NgModule({
