adb55c59bba5311c5d3513028ca7ea9f1dca45c5
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-21 10:16:13 +0200

    Consolidating Home and Landing Pages into single Main Menu Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { OrderRoundPageComponent } from './pages/order-round-page/order-round-page.component';
 import { SettleTabPageComponent } from './pages/settle-tab-page/settle-tab-page.component';
+import { MainMenuPageComponent } from './pages/main-menu-page/main-menu-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    HomePageComponent,
-    ButtonComponent,
+
+    // Pages
+    MainMenuPageComponent,
     OrderRoundPageComponent,
-    SettleTabPageComponent
+    SettleTabPageComponent,
+
+    // Shared component
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
