a476b7da748f60023d1b84166aca839c9bb572be
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-22 22:54:03 +0200

    Setting up views for Open Tab Page

---
+++ src/app/pages/open-tab/views/open-tab-confirmation/open-tab-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-tab-confirmation',
+  templateUrl: './open-tab-confirmation.component.html',
+  styleUrls: ['./open-tab-confirmation.component.scss']
+})
+export class OpenTabConfirmationComponent {
+
+}
---
+++ src/app/pages/open-tab/views/open-tab-confirmed/open-tab-confirmed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-tab-confirmed',
+  templateUrl: './open-tab-confirmed.component.html',
+  styleUrls: ['./open-tab-confirmed.component.scss']
+})
+export class OpenTabConfirmedComponent {
+
+}
