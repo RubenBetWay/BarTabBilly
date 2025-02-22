3d939eeb26d0d99efc1d57b687e153acbb9a505c
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-22 23:54:43 +0200

    Can now open a tab - for Just me at least

--- src/app/pages/open-tab/views/open-tab-confirmation/open-tab-confirmation.component.ts
+++ src/app/pages/open-tab/views/open-tab-confirmation/open-tab-confirmation.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonConfig } from 'src/app/shared/components/button/button.model';
+import { ConfirmButton, RejectButton } from './open-tab-confirmation.const';
 
 @Component({
   selector: 'app-open-tab-confirmation',
   templateUrl: './open-tab-confirmation.component.html',
-  styleUrls: ['./open-tab-confirmation.component.scss']
+  styleUrls: ['./open-tab-confirmation.component.scss'],
 })
 export class OpenTabConfirmationComponent {
   @Input() isJustMe: boolean = false;
-
+  @Output() onAnswer: EventEmitter<string> = new EventEmitter();
+  
+  options: ButtonConfig[] = [ConfirmButton, RejectButton];
 }
