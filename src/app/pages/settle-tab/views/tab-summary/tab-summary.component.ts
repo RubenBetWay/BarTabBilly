d3dd7a2c46296a4e49654b15f106c56f89bef50a
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-24 04:55:56 +0200

    Progress made on settle tab page

---
+++ src/app/pages/settle-tab/views/tab-summary/tab-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-summary',
+  templateUrl: './tab-summary.component.html',
+  styleUrls: ['./tab-summary.component.scss']
+})
+export class TabSummaryComponent {
+
+}
