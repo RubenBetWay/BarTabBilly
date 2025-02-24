5caf7b2b6f6dfff3a3563cf9f73a0daed9248f1f
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-24 11:50:59 +0200

    Settlement confirmation view completed

---
+++ src/app/pages/settle-tab/views/settlement-confirmed/settlement-confirmed.const.ts
@@ -0,0 +1,8 @@
+import { ButtonConfig, ButtonSize } from "src/app/shared/components/button/button.model";
+
+export const ConfirmedButton: ButtonConfig = {
+  name: 'home',
+  text: 'Cool',
+  size: ButtonSize.Wide,
+  color: 'aquamarine',
+};
