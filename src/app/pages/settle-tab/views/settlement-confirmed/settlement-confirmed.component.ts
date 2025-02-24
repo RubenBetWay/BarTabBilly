d76322b852ae72b94c4bf17c16f196434e2fbd5d
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-24 11:06:50 +0200

    Settlement confirmed and Tab Split views generated

---
+++ src/app/pages/settle-tab/views/settlement-confirmed/settlement-confirmed.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settlement-confirmed',
+  templateUrl: './settlement-confirmed.component.html',
+  styleUrls: ['./settlement-confirmed.component.scss']
+})
+export class SettlementConfirmedComponent {
+
+}
---
+++ src/app/pages/settle-tab/views/tab-split/tab-split.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-split',
+  templateUrl: './tab-split.component.html',
+  styleUrls: ['./tab-split.component.scss']
+})
+export class TabSplitComponent {
+
+}
