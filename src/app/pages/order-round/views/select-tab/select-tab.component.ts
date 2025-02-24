dffcc53b2d0c8326943315392d9bfca1cfc961bf
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2025-02-24 04:00:09 +0200

    Order Round: Adding select tab view and tab card

---
+++ src/app/pages/order-round/views/select-tab/select-tab.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TabData } from 'src/app/shared/services/data/data.model';
+import { DataService } from 'src/app/shared/services/data/data.service';
+
+@Component({
+  selector: 'app-select-tab',
+  templateUrl: './select-tab.component.html',
+  styleUrls: ['./select-tab.component.scss']
+})
+export class SelectTabComponent {
+  @Output() tabSelected: EventEmitter<string> = new EventEmitter();
+  
+  openTabs: TabData[] = [];
+
+  constructor(private dataService: DataService) {
+    // Should not be able to access this page if
+    // there are no unsettled tabs - so we are
+    // just going to place guards in place
+    if (!dataService.data) return;
+    if (!dataService.data.tabs) return;
+    this.openTabs = dataService.data.tabs.filter(
+      (tab: TabData) => !tab.isSettled
+    );
+    console.log(this.openTabs)
+  }
+}
